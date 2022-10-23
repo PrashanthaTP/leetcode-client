1d72b9dd3d47472676231cc2be37bf480aafd46c
PrashanthaTP <tpprashantha@gmail.com>
2022-10-23 23:27:15 +0530

    Clean: Create util for getting dirname

--- config/config.js
+++ config/config.js
@@ -1,12 +1,8 @@
 import { readFileSync } from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+import { getDirname } from "../utils";
 
-//Node.js 10 supports ECMAScript modules, where __dirname and __filename are no longer available.
-//https://stackoverflow.com/a/50053801/12988588
-//https://stackoverflow.com/a/55859500/12988588
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+const __dirname = getDirname(import.meta.url)
 const configFilePath = path.resolve(__dirname, "../.env.json");
 const configDataRaw = readFileSync(configFilePath);
 const config = JSON.parse( configDataRaw );
---
+++ utils/index.js
@@ -0,0 +1,10 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+//Node.js 10 supports ECMAScript modules, where __dirname and __filename are no longer available.
+//https://stackoverflow.com/a/50053801/12988588
+//https://stackoverflow.com/a/55859500/12988588
+export const getDirname = ( filepath ) => {
+	return  path.dirname(fileURLToPath(import.meta.url));
+}
+
